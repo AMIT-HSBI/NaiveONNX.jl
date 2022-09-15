ccce7d53bf23132d3eea57c61d9716593891100b
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2022-09-15 11:04:54 +0200

    Silence Logging.info in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
 using SafeTestsets
+using Logging
+Logging.disable_logging(Logging.Info)
 
-#@safetestset "Data Visualization" begin include("visualizationTests.jl") end
+@safetestset "Data Visualization" begin include("visualizationTests.jl") end
 @safetestset "Data Visualization" begin include("onnxGenTests.jl") end
