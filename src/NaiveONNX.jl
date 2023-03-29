2d27c794e53072772b69b6dfe3d1a4d0b5a8e6bf
AnHeuermann <andreas.heuermann@fh-bielefeld.de>
2023-03-29 11:44:41 +0200

    Remove duplicate definition of sigmoid

--- src/NaiveONNX.jl
+++ src/NaiveONNX.jl
@@ -10,10 +10,6 @@ import ONNXNaiveNASflux
 import Plots
 import StatsBase
 
-# Add Flux.sigmoid operation for ONNXNaiveNASflux
-Flux.sigmoid(pp::ONNXNaiveNASflux.AbstractProbe) = ONNXNaiveNASflux.attribfun(identity, "Sigmoid", pp)
-ONNXNaiveNASflux.refresh()
-
 include("main.jl")
 export trainONNX
 export visualizeData
